d3b4448695bcb755eb7d10996cfc9784789a01f8
felipenagai <125044244+felipe-ks-nagai@users.noreply.github.com>
2026-04-01 17:07:14 -0300

    Add files via upload

---
+++ Atividade2/Empregado/Mensalista.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empregado
+{
+    internal class Mensalista : Empregado
+    {
+        public double SalarioMensal {  get; set; }
+
+        public override double SalarioBruto()
+        {
+            return SalarioMensal;
+        }
+
+        public Mensalista()
+        {
+            System.Windows.Forms.MessageBox.Show("Aqui é mensalista");
+        }
+
+        public Mensalista(int matx, string nomex, DateTime datax, double salx)
+        {
+            this.NomeEmpregado = nomex;
+            this.Matricula = matx;
+            this.DataEntradaEmpresa = datax;
+            this.SalarioMensal = salx;
+        }
+    }
+}
