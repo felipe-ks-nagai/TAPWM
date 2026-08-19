4bdd3d5b91cac2cc19c151ad6f13dd1361e6281b
felipenagai <125044244+felipe-ks-nagai@users.noreply.github.com>
2026-08-19 14:28:56 -0300

    Add files via upload

---
+++ Atividade17/callback2.js
@@ -0,0 +1,8 @@
+function exibeMensagensNaOrden(mensagem, callback) {
+    console.log(mensagem);
+    callback;
+}
+
+exibeMensagensNaOrden('Essa é a primeira mensagem exibida na ordem', function(){
+    console.log('Essa é a segunda mensagem exibida na ordem');
+})
